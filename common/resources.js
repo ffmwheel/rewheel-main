fac18a7970a5ac36acedabc3b122285500c1b775
Camwp <camwpedro@gmail.com>
2024-03-13 16:43:31 -0700

    ipfs link update

--- common/resources.js
+++ common/resources.js
@@ -6,6 +6,6 @@ export const resources = {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
   "OTA Files Pint / GT & Firmware (IMPORTANT! REQUIRES IPFS COMPATIBLE BROWSER. RECOMMENDED: 'FIREFOX')": {
-    "sourceLink": "https://bafybeifllzsk35d5skjxjh3wm24cu3ngevtcxc3xstxudcncm6wer6q3y4.ipfs.w3s.link/"
+    "sourceLink": "https://jade-competitive-dingo-303.mypinata.cloud/ipfs/QmVHdec2jDvNkMgpvRrm5uctKJGLd6sk9tcgg7YYaWAfYa"
   },
 }
