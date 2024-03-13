68334c855439bb399bfe8874bffea43452fe1f9c
Camwp <camwpedro@gmail.com>
2024-03-13 16:40:47 -0700

    BROWSER RECOMMENDATION

--- common/resources.js
+++ common/resources.js
@@ -5,7 +5,7 @@ export const resources = {
   "Shadowwheel(Local version)": {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
-  "OTA Files Pint / GT & Firmware": {
+  "OTA Files Pint / GT & Firmware (IMPORTANT! REQUIRES IPFS COMPATIBLE BROWSER. RECOMMENDED: 'FIREFOX')": {
     "sourceLink": "https://bafybeifllzsk35d5skjxjh3wm24cu3ngevtcxc3xstxudcncm6wer6q3y4.ipfs.w3s.link/"
   },
 }
